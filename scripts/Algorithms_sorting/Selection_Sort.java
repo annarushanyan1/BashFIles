1463ff9803db70abf7fcd6dcf3a539b9c66f053c
annarushanyan1 <arushanyananna-5@asu.am>
2022-01-24 21:31:00 +0400

    selection sort

--- scripts/Algorithms_sorting/Selection_Sort.java
+++ scripts/Algorithms_sorting/Selection_Sort.java
@@ -3,60 +3,26 @@ package ITC.scripts.Algorithms_sorting;
 import java.util.Arrays;
 
 public class Selection_Sort {
-
     public static void main(String[] args) {
         int[] array = {5, 2, 3, 6, 32, 9, 54, 2, 3, 6, 30};
-        int[] sorted_array = sorting(array);
+        int[] sorted_array = SelectionSort(array);
         System.out.println(Arrays.toString(sorted_array));
     }
 
-    public static int[] sorting(int[] arr) {
-        int len = arr.length;
+    public static int[] SelectionSort(int[] arr) {
 
-        if(len == 1){
-            return arr;
-        }
-        int[] copy = new int[len];
-
-        for (int i = 0; i < len; i++) {
-            int[] someArray = findMin(arr);
-            copy[i] = someArray[0];
-            arr = removeTheElement(arr, someArray[1]);
-        }
-        return copy;
-    }
-
-    public static int[] findMin(int[] inputArray) {
-        int index = 0;
-        int minValue = inputArray[index];
-        for (int i = 1; i < inputArray.length; i++) {
-            if (inputArray[i] < minValue) {
-                minValue = inputArray[i];
-                index = i;
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
             }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
         }
-        return new int[]{minValue, index};
+        return arr;
     }
-
-    public static int[] removeTheElement(int[] arr, int index) {
-
-        if (arr == null || index < 0 || index >= arr.length) {
-            return arr;
-        }
-
-        int[] anotherArray = new int[arr.length - 1];
-
-        for (int i = 0, k = 0; i < arr.length; i++) {
-
-            if (i == index) {
-                continue;
-            }
-
-            anotherArray[k++] = arr[i];
-        }
-
-        return anotherArray;
-    }
-
-
 }
