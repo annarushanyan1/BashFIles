2080b7391adf025fff2127b16cdc80de3141642c
annarushanyan1 <arushanyananna-5@asu.am>
2022-04-24 23:58:50 +0400

    deleted files

--- linux.sh
+++
@@ -1,7 +0,0 @@
-# ԽՆԴՐԻ ՆԿԱՐԱԳՐՈՒԹՅՈՒՆԸ ՏԵՍ ՆԵՐՔԵՎՈՒՄ
-# Գրել bash script, որը տրված դիրեկտորիայից տպում է միայն այն ֆայլերի անունները,
-# որոնք պարունակում են ITC տեքստը։
-# Խնդիրը սկսելուց առաջ համոզվիր որ պահանջը պարզ է։
-
-ls | grep "ITC"
-# to search in other directory you need to add path after ls "ls /usr/somewhere | grep "ITC" "
